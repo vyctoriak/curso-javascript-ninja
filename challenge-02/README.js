02c0cbd15427e83e77b7939e85e1add1240b213e
Vyctoria Karina <vyctoriak@gmail.com>
2025-03-16 21:30:02 -0300

    feat: challenge 02 solved

---
+++ challenge-02/README.js
@@ -0,0 +1,101 @@
+// # Desafio da semana #2
+
+// Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções! :smile:
+
+// // Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+
+function soma(a, b) {
+    return a + b;
+}
+
+// // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+
+var resultado = soma(4 + 1);
+
+// // Qual o valor atualizado dessa variável?
+
+console.log(resultado);
+
+// // Declare uma nova variável, sem valor.
+
+var empty;
+
+// /*
+// Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+//     O valor da variável agora é VALOR.
+// Onde VALOR é o novo valor da variável.
+// */
+
+function addNumber(a) {
+    return `O valor da variável agora é ${a + empty}.`;
+}   
+
+// // Invoque a função criada acima.
+
+var adicionandoNumero = addNumber(10);
+
+// // Qual o retorno da função? (Use comentários de bloco).
+
+console.log(adicionandoNumero);
+
+// /*
+// Crie uma função com as seguintes características:
+// 1. A função deve receber 3 argumentos;
+// 2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+//     Preencha todos os valores corretamente!
+// 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+// */
+
+function multiplicacao(a, b, c) {
+    if(a === undefined || b === undefined || c === undefined ) {
+        return `Preencha todos os valores corretamente!`;
+    } else {
+        return ((a * b * c) + 2);
+    }
+};
+
+
+// // Invoque a função criada acima, passando só dois números como argumento.
+var resultadoMulti = multiplicacao(2, 3);
+
+// // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+console.log(resultadoMulti);
+
+// // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+
+var multiplicacao = multiplicacao(2, 3, 4);
+
+// // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+
+console.log(multiplicacao);
+
+// /*
+// Crie uma função com as seguintes características:
+// 1. A função deve receber 3 argumentos.
+// 2. Se somente um argumento for passado, retorne o valor do argumento.
+// 3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+// 4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+// 5. Se nenhum argumento for passado, retorne o valor booleano `false`.
+// 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
+// */
+
+function tresArgumentos(a, b, c) {
+    if(a !== undefined && b === undefined && c === undefined) {
+        return a;
+    } else if (a !== undefined && b !== undefined && c === undefined) {
+        return a + b;
+    } else if (a !== undefined && b !== undefined && c !== undefined) {
+        return ((a + b) / c);
+    } else if (a === undefined && b === undefined && c === undefined) {
+        return false;
+    } else {
+        return null;
+    }
+}
+
+// // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+
+console.log(tresArgumentos()) // false
+console.log(tresArgumentos(2))  // 2
+console.log(tresArgumentos(2, 3)) // 5
+console.log(tresArgumentos(2, 3, 4)) // 1.25
